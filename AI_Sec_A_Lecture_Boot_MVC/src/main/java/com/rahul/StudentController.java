fe43fab74089475f03801440a3e37c04301c84ef
rahul <posttorahuldixit@gmail.com>
2024-07-02 11:16:01 +0530

    AI_Sec_A ThymeleafSave 02:07:2024

--- AI_Sec_A_Lecture_Boot_MVC/src/main/java/com/rahul/StudentController.java
+++ AI_Sec_A_Lecture_Boot_MVC/src/main/java/com/rahul/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,14 +27,21 @@ public class StudentController {
 		model.addAttribute("list_of_students", service.reteriveAll());
 		return "index";
 	}
+	@GetMapping(path = {"/addNew"})
+	public String addNewStudent(Model model) {
+		Student student = new Student(000000,"Please Enter Student Name",00000000000L);
+		model.addAttribute("student",student);
+		return "addStudent";
+	}
 	@GetMapping("/students/{id}")
 	public String getStudentById(@PathVariable int id,Model model ) {
 		model.addAttribute("student", service.reteriveById(id));
 		return "demo";
 	}
-	@PostMapping("/student")
-	public void storeStudent(@RequestBody Student student) {
+	@PostMapping("/save")
+	public String storeStudent(@ModelAttribute("student") Student student) {
 		service.saveStudent(student);
+		return "redirect:/";
 	}
 	@DeleteMapping("/students/{id}")
 	public void removeStudentById(@PathVariable int id ) {
