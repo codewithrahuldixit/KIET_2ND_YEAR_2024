4369b214d91bf2b2eed78567c2cf0a9a88d3377e
rahul <posttorahuldixit@gmail.com>
2024-07-04 10:28:14 +0530

    Ai_Sec_C Final 07:04:2024

--- AI_Sec_C_Lecture_BootMVC_01/src/main/java/com/rahul/StudentController.java
+++ AI_Sec_C_Lecture_BootMVC_01/src/main/java/com/rahul/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,25 +23,36 @@ public class StudentController {
 		this.service = service;
 	}
 
-	@GetMapping("/students/{id}")
+	@GetMapping("/updateStudent/{id}")
 	public String getStudent(@PathVariable int id,Model mav) {
 	Student stud =  service.reteriveByID(id);
 	mav.addAttribute("student",stud);
-	return "demo";
+	return "addStudent";
 	}
 
-	@DeleteMapping("/students/{id}")
-	public void deleteStudent(@PathVariable int id) {
+	@GetMapping("/deleteStudent/{id}")
+	public String deleteStudent(@PathVariable int id) {
 		service.removeByid(id);
+		return "redirect:/";
 	}
 
-	@GetMapping("/students")
-	public List<Student> getAllStudent() {
-		return service.reteriveAll();
+	@GetMapping(path = {"/","students"})
+	public String getAllStudent(Model model) {		
+		model.addAttribute("list_of_students", service.reteriveAll());
+		//it Returns The HTML Page Name
+		return "index";
+	}
+	@GetMapping(path = {"/addNew"})
+	public String newStudent(Model model) {	
+		Student student = new Student(0,"Enter Name",00000000l);
+		model.addAttribute("student", student);
+		//it Returns The HTML Page Name
+		return "addStudent";
 	}
 
-	@PostMapping("/student")
-	public void getStudent(@RequestBody Student student) {
+	@PostMapping("/save")
+	public String getStudent(@ModelAttribute("student") Student student) {
 		service.saveStudent(student);
+		return "redirect:/";
 	}
 }
